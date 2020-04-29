88b3b048451c5083932e859e943a301d43e399e8
Dmitriy Lyalyuev <dmitriy@lyalyuev.info>
2020-04-29 21:30:05 +0300

    typo: use braces in variables

--- backup.sh
+++ backup.sh
@@ -5,13 +5,13 @@ source ${DIR}/mikrotik_backup.settings
 
 for r in ${routers[@]}; do
     cmd_backup="/system backup save name=${r}.backup"
-    ssh ${login}@$r -p $port -i $privatekey "${cmd_backup}" > /dev/null
+    ssh ${login}@$r -p ${port} -i ${privatekey} "${cmd_backup}" > /dev/null
     cmd_backup="/export file=${r}"
-    ssh ${login}@$r -p $port -i $privatekey "${cmd_backup}" > /dev/null
+    ssh ${login}@$r -p ${port} -i ${privatekey} "${cmd_backup}" > /dev/null
     sleep 5
-    mkdir -p $fulldir
-    wget -qP $fulldir ftp://${login}:${passwd}@${r}/${r}.backup
-    wget -qP $fulldir ftp://${login}:${passwd}@${r}/${r}.rsc
-    ssh ${login}@$r -p $port -i $privatekey "/file remove \"${r}.backup\""
-    ssh ${login}@$r -p $port -i $privatekey "/file remove \"${r}.rsc\""
+    mkdir -p ${fulldir}
+    wget -qP ${fulldir} ftp://${login}:${passwd}@${r}/${r}.backup
+    wget -qP ${fulldir} ftp://${login}:${passwd}@${r}/${r}.rsc
+    ssh ${login}@$r -p ${port} -i ${privatekey} "/file remove \"${r}.backup\""
+    ssh ${login}@$r -p ${port} -i ${privatekey} "/file remove \"${r}.rsc\""
 done
